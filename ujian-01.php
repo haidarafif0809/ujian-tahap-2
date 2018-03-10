50ce9433eb7e51ede5b15ca81371d88ab58d9dec
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2018-03-10 14:10:44 +0700

    jawaban

---
+++ ujian-01.php
@@ -0,0 +1,15 @@
+<?php 
+$message = 'untuk menjadi programmer yang handal diperlukan pengorbanan yang besar dan usaha yang keras. programmer itu bukan soal koding, tapi soal memberikan solusi. perlu diingat selalu, programmer itu membuat kode bukan hanya untuk bisa di mengerti oleh komputer, tapi juga harus dimengerti oleh programmer lain';
+
+function test($message)
+{
+	switch ($message){
+		case "programmer":
+
+		break;
+		case ""
+	}
+}
+
+
+?>
